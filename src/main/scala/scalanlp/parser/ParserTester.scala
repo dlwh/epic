7a620cc5f1a03b3f1977810336bb41d8f48a7b9c
David Hall <dlwh@berkeley.edu>
2010-03-29 12:56:07 -0700

    Moved ParserTester to its own file.

---
+++ src/main/scala/scalanlp/parser/ParserTester.scala
@@ -0,0 +1,58 @@
+package scalanlp.parser;
+
+import java.io.File
+import java.io.FileInputStream
+import java.util.Properties
+import scalanlp.config.Configuration
+import scalanlp.trees.BinarizedTree
+import scalanlp.trees.DenseTreebank
+import scalanlp.trees.Tree
+import scalanlp.trees.Treebank
+import scalanlp.trees.Trees
+
+trait ParserTester {
+  def trainParser(trainTrees: Iterable[(BinarizedTree[String],Seq[String])],
+                  devTrees: Iterable[(BinarizedTree[String],Seq[String])],
+                  config: Configuration):Parser[String,String];
+
+  def main(args: Array[String]) {
+    val properties = new Properties();
+    properties.load(new FileInputStream(new File(args(0))));
+    val config = Configuration.fromProperties(properties);
+
+    val treebank = {
+      val path = config.readIn[File]("treebank.path");
+      if(path.isDirectory) Treebank.fromPennTreebankDir(path)
+      else DenseTreebank.fromZipFile(path);
+    }
+
+    val binarize = {
+      val kind = config.readIn[String]("tree.binarization","standard");
+      if(kind == "xbar") Trees.xBarBinarize _ ;
+      else Trees.binarize(_:Tree[String]);
+    }
+
+    val maxLength = config.readIn[Int]("sentence.maxLength",40);
+
+    val xform = Trees.Transforms.StandardStringTransform;
+
+    val trainTrees = for( (tree,words) <- treebank.trainTrees)
+      yield (binarize(xform(tree)),words map (_.intern));
+
+    val devTrees = for( (tree,words) <- treebank.devTrees)
+      yield (binarize(xform(tree)),words map (_.intern));
+
+    println("Training Parser...");
+    val parser = trainParser(trainTrees,devTrees,config);
+
+
+    println("Evaluating Parser...");
+    val testTrees = treebank.testTrees.iterator.filter(_._2.length <= maxLength);
+    val (prec,recall,exact) = ParseEval.evaluate(testTrees,parser,xform);
+    val f1 = (2 * prec * recall)/(prec + recall);
+    println("Eval finished. Results:");
+    println( "P: " + prec + " R:" + recall + " F1: " + f1 +  " Ex:" + exact);
+  }
+
+}
+
