365d8e579e81990c9bad3814717d765bc966f186
David Hall <dlwh@berkeley.edu>
2010-10-03 11:16:06 -0700

    discrimobjective fleshed out and compiling.

--- src/main/scala/scalanlp/parser/discrim/DiscrimObjective.scala
+++ src/main/scala/scalanlp/parser/discrim/DiscrimObjective.scala
@@ -6,30 +6,78 @@ import scalala.tensor.sparse.SparseVector
 import scalanlp.optimize.DiffFunction
 import scalanlp.trees._;
 import InsideOutside._;
+import ParseChart.LogProbabilityParseChart;
+import scalanlp.concurrent.ParallelOps._;
+import scalanlp.concurrent.ThreadLocal;
+import scalala.Scalala._;
+import scalala.tensor.counters.Counters._
 
 /**
  * 
  * @author dlwh
  */
-class DiscrimObjective[L,W] extends DiffFunction[Int,DenseVector] {
+class DiscrimObjective[L,W](feat: Featurizer[L,W],
+                            root: L,
+                            trees: IndexedSeq[(BinarizedTree[L],Seq[W])]) extends DiffFunction[Int,DenseVector] {
   def calculate(weights: DenseVector) = {
-    val grad = weights.like;
 
-    (Double.NegativeInfinity,grad);
+    val grammar = weightsToGrammar(weights);
+    val lexicon = weightsToLexicon(weights);
+    val parser = new CKYParser[LogProbabilityParseChart,L,W](root,lexicon,grammar,ParseChart.logProb);
+    val ecounts = indexedTrees.par.fold(new ExpectedCounts[W](grammar)) { (counts, treewords) =>
+      val tree = treewords._1;
+      val words = treewords._2;
+
+      val treeCounts = treeToExpectedCounts(grammar,lexicon,tree,words);
+      val wordCounts = wordsToExpectedCounts(words, parser);
+      counts += treeCounts -= wordCounts;
+
+    } { (ecounts1, ecounts2) =>
+      ecounts1 += ecounts2
+    }
+
+    val grad = -expectedCountsToFeatureVector(ecounts)
+
+    (ecounts.logProb, grad value);
   }
 
-  val indexedFeatures: FeatureIndexer[L,W] = null;
+  val indexedFeatures: FeatureIndexer[L,W] = FeatureIndexer(feat,trees);
+  val indexedTrees = for( (t,w) <- trees) yield (t.map(indexedFeatures.labelIndex),w);
 
-  def weightsToGrammar(weights: DenseVector) = {
-    val grammar = new FeaturizedGrammar(weights, indexedFeatures);
+  val openTags = Set.empty ++ {
+    val lexicon = new PairedDoubleCounter[L,W]();
+    val tags = collection.mutable.Set[L]();
+    for{
+      (tree,words) <- trees
+      leaves = tree.leaves map (l => (l,words(l.span.start)));
+      (l,w) <- leaves
+    }{
+      tags += l.label;
+      lexicon(l.label).incrementCount(w,1);
+    }
+    for( t <- tags if lexicon(t).size > 50)  yield t;
+  }
+
+  def weightsToLexicon(weights: DenseVector) = {
+    val grammar = new FeaturizedLexicon(openTags, weights, indexedFeatures);
     grammar;
   }
 
+  def weightsToGrammar(weights: DenseVector):Grammar[L] = {
+    val grammar =  new FeaturizedGrammar(weights,indexedFeatures)
+    grammar;
+  }
+
+  def wordsToExpectedCounts(words: Seq[W], parser: ChartParser[LogProbabilityParseChart,L,W]) = {
+    val ecounts = new InsideOutside(parser).expectedCounts(words);
+    ecounts
+  }
+
   // these expected counts are in normal space, not log space.
-  private def treeToExpectedCountsAndScore(g: Grammar[L],
+  private def treeToExpectedCounts(g: Grammar[L],
                                    lexicon: Lexicon[L,W],
                                    t: BinarizedTree[Int],
-                                   words: Seq[W]):(ExpectedCounts[W],Double) = {
+                                   words: Seq[W]):ExpectedCounts[W] = {
     val expectedCounts = new ExpectedCounts[W](g)
     var score = 0.0;
     for(t2 <- t.allChildren) {
@@ -46,7 +94,33 @@ class DiscrimObjective[L,W] extends DiffFunction[Int,DenseVector] {
           score += lexicon.wordScore(g.index.get(a), w);
       }
     }
-    (expectedCounts,score);
+    expectedCounts.logProb = score;
+    expectedCounts;
+  }
+
+  def expectedCountsToFeatureVector(ecounts: ExpectedCounts[W]):DenseVector = {
+    val result = indexedFeatures.mkDenseVector(0.0);
+
+    // binaries
+    for( (a,bvec) <- ecounts.binaryRuleCounts;
+         (b,cvec) <- bvec;
+         (c,v) <- cvec.activeElements) {
+      result += (indexedFeatures.featuresFor(a,b,c) * v);
+    }
+
+    // unaries
+    for( (a,bvec) <- ecounts.unaryRuleCounts;
+         (b,v) <- bvec.activeElements) {
+      result += (indexedFeatures.featuresFor(a,b) * v);
+    }
+
+    // lex
+    for( (a,ctr) <- ecounts.wordCounts;
+         (w,v) <- ctr) {
+      result += (indexedFeatures.featuresFor(a,w) * v);
+    }
+
+    result;
   }
 
 
