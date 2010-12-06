7a09cd4089d53f8fdfee3ff0992b50c0b69ef405
David Hall <dlwh@berkeley.edu>
2010-12-05 18:53:59 -0800

    added signatures to wordshape featurizer.

--- src/main/scala/scalanlp/parser/discrim/WordShapeFeaturizer.scala
+++ src/main/scala/scalanlp/parser/discrim/WordShapeFeaturizer.scala
@@ -3,24 +3,26 @@ package scalanlp.parser.discrim
 import scalala.tensor.counters.Counters
 import scalala.tensor.counters.Counters.PairedDoubleCounter
 import collection.mutable.ArrayBuffer
-import scalanlp.parser.Rule;
+import scalanlp.parser.Rule
+import java.nio.CharBuffer;
 
 
 sealed abstract class WordShapeFeature[+L](l: L) extends Feature[L,Nothing];
 final case class IndicatorWSFeature[L](l: L, name: Symbol) extends WordShapeFeature(l);
 final case class SuffixFeature[L](l: L, str: String) extends WordShapeFeature(l);
+final case class ShapeFeature[+L](l: L, str: String) extends WordShapeFeature(l);
 
 /**
  * This class generates features according to the word shapes.
  */
-class WordShapeFeaturizer[L](lexicon: PairedDoubleCounter[L,String], initToZero: Boolean = false) extends Featurizer[L,String] {
+class WordShapeFeaturizer[L](lexicon: PairedDoubleCounter[L,String], initToZero: Boolean = true) extends Featurizer[L,String] {
   val wordCounts = Counters.aggregate(for( ((_,w),count) <- lexicon.activeElements) yield (w,count));
 
   def initialValueForFeature(f: Feature[L,String]) = f match {
     case LexicalFeature(l:L,w:String) if !initToZero =>
       val r = math.log(lexicon(l,w)/lexicon(l).total);
       if(r == Double.NegativeInfinity) -10
-      else r/100
+      else r
     case _ => 0.0;
   }
 
@@ -30,7 +32,10 @@ class WordShapeFeaturizer[L](lexicon: PairedDoubleCounter[L,String], initToZero:
     if(wordCounts(w) > 5) Counters.aggregate(LexicalFeature(l,w) -> 1.0);
     else {
       val features = ArrayBuffer[Feature[L,String]]();
-      features += LexicalFeature(l,w);
+      if(math.random < 0.5 || wordCounts(w) > 3) // really rare words should randomly be removed the treebank.
+        features += LexicalFeature(l,w);
+
+      features += makeShapeFeature(l, w);
 
       val wlen = w.length;
       val numCaps = (w:Seq[Char]).count{_.isUpper};
@@ -40,6 +45,7 @@ class WordShapeFeaturizer[L](lexicon: PairedDoubleCounter[L,String], initToZero:
       val hasNonDigit = hasLetter || w.exists(!_.isDigit);
       val hasLower = w.exists(_.isLower);
       val hasDash = w.contains('-');
+
       if(numCaps > 0) features += (IndicatorWSFeature(l,'HasCap));
       if(numCaps > 1) features += (IndicatorWSFeature(l,'HasManyCap));
       if(numCaps > 1 && !hasLower) features += (IndicatorWSFeature(l,'AllCaps));
@@ -49,7 +55,8 @@ class WordShapeFeaturizer[L](lexicon: PairedDoubleCounter[L,String], initToZero:
         if(wordCounts(w.toLowerCase) > 3 && wordCounts(w) <= 3) {
           features += IndicatorWSFeature(l,'HasKnownLC);
           if(lexicon(l,w.toLowerCase) > 0)
-            features += LexicalFeature(l,w.toLowerCase);
+            features += IndicatorWSFeature(l,'WordOccursAsLCWithTag)
+          else features += IndicatorWSFeature(l,'WordDoesNotOccurAsLCWithTag)
         }
       }
       if(!hasLower) features += (IndicatorWSFeature(l,'HasNoLower));
@@ -57,6 +64,18 @@ class WordShapeFeaturizer[L](lexicon: PairedDoubleCounter[L,String], initToZero:
       if(hasDigit) features += (IndicatorWSFeature(l,'HasDigit));
       if(!hasLetter) features += (IndicatorWSFeature(l,'HasNoLetter));
       if(hasNotLetter) features += (IndicatorWSFeature(l,'HasNotLetter));
+
+      if(hasDash && (hasLetter || hasDigit))  {
+        val split = w.split("-");
+        val lastInSplit = split.last;
+        if(lexicon(l)(lastInSplit) > 0) {
+          features += IndicatorWSFeature(l,'PostHyphenOccursWithLabel)
+        } else if(wordCounts(lastInSplit) > 0) {
+          features += IndicatorWSFeature(l,'PostHyphenDoesNotOccurWithLabel)
+        }
+      }
+
+
 
       if(w.length > 3 && w.endsWith("s") && !w.endsWith("ss") && !w.endsWith("us") && !w.endsWith("is"))
          features += (IndicatorWSFeature(l,'EndsWithS));
@@ -73,6 +92,24 @@ class WordShapeFeaturizer[L](lexicon: PairedDoubleCounter[L,String], initToZero:
       Counters.aggregate(features.iterator.map(f => (f,1.0)));
     }
 
+  }
+
+  def makeShapeFeature(l: L, word: String) = {
+    val result = new StringBuilder(word.length);
+    var i = 0;
+    while(i < word.length) {
+      val c = word(i);
+      val x = if(c.isLetter && c.isUpper) 'X' else if(c.isLetter) 'x' else if(c.isDigit) 'd' else c;
+      if(result.length > 1 && (result.last == x) && result(result.length - 2) == x) {
+        result += 'e'
+      } else if (result.length > 1 && result.last == 'e' && result(result.length - 2) == x) {
+        () // nothing
+      }else {
+        result += x;
+      }
+      i +=1;
+    }
+    new ShapeFeature(l,result.toString.intern);
   }
 
 }
