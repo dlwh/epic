f71ecc0457ccc391713fc9cfc3acca160f33233b
David Hall <dlwh@berkeley.edu>
2010-12-16 19:52:13 -0800

    forgot a file.

---
+++ src/main/scala/scalanlp/parser/LabelScoreArray.scala
@@ -0,0 +1,35 @@
+package scalanlp.parser
+
+import scalanlp.collection.mutable.TriangularArray
+import java.util.Arrays
+import scalala.tensor.dense.DenseVector
+
+/**
+ * 
+ * @author dlwh
+ */
+@serializable
+@SerialVersionUID(1)
+class LabelScoreArray(length: Int, grammarSize: Int, fill: Double) {
+  protected val score = TriangularArray.raw(length+1, LabelScoreArray.mkGrammarVector(grammarSize, fill));
+  protected val enteredLabels = TriangularArray.raw(length+1,new collection.mutable.BitSet());
+
+  final def apply(begin: Int, end: Int, label: Int) = labelScore(begin,end,label);
+  final def labelScore(begin: Int, end: Int, label: Int) = score(TriangularArray.index(begin,end))(label);
+  final def enteredLabelIndexes(begin: Int, end: Int) = {
+    enteredLabels(TriangularArray.index(begin,end)).iterator;
+  }
+
+  def enteredLabelScores(begin: Int, end: Int) = {
+    val scoreArray = score(TriangularArray.index(begin,end));
+    enteredLabels(TriangularArray.index(begin,end)).iterator.map { i => (i,scoreArray(i))};
+  }
+}
+
+object LabelScoreArray {
+  private def mkGrammarVector(grammarSize: Int, fill: Double) = {
+    val r = new DenseVector(grammarSize);
+    Arrays.fill(r.data,fill);
+    r
+  }
+}
