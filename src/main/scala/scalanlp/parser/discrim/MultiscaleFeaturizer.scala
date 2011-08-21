0225bee2ad23aa41cf19e321225d91c5b1ee02ad
David Hall <dlwh@berkeley.edu>
2011-08-21 14:29:15 -0700

    more noise is better.

--- src/main/scala/scalanlp/parser/discrim/MultiscaleFeaturizer.scala
+++ src/main/scala/scalanlp/parser/discrim/MultiscaleFeaturizer.scala
@@ -17,7 +17,7 @@ class MultiscaleFeaturizer[L,W](base: Featurizer[L,W]) extends Featurizer[(L,Seq
       val baseFeatures = base.featuresFor(BinaryRule(a._1,b._1,c._1))
       for( (k,v) <- baseFeatures.nonzero.pairs) {
         var i = 0;
-        while(i <= a._2.length) {
+        while(i < a._2.length) {
           val substates = ArrayBuffer(a._2.drop(i),b._2.drop(i),c._2.drop(i))
           result(SubstateFeature(k,substates)) = v
           i += 1
@@ -29,7 +29,7 @@ class MultiscaleFeaturizer[L,W](base: Featurizer[L,W]) extends Featurizer[(L,Seq
       val baseFeatures = base.featuresFor(UnaryRule(a._1,b._1))
       for( (k,v) <- baseFeatures.nonzero.pairs) {
         var i = 0;
-        while(i <= a._2.length) {
+        while(i < a._2.length) {
           val substates = ArrayBuffer(a._2.drop(i),b._2.drop(i))
           result(SubstateFeature(k,substates)) = v
           i += 1
@@ -44,7 +44,7 @@ class MultiscaleFeaturizer[L,W](base: Featurizer[L,W]) extends Featurizer[(L,Seq
     val result = Counter[Feature[(L,Seq[Int]),W],Double]()
     for( (k,v) <- baseFeatures.nonzero.pairs) {
       var i = 0;
-      while(i <= l._2.length) {
+      while(i < l._2.length) {
         val substates = ArrayBuffer(l._2.drop(i))
         result(SubstateFeature(k,substates)) = v
         i += 1
@@ -58,8 +58,7 @@ class MultiscaleFeaturizer[L,W](base: Featurizer[L,W]) extends Featurizer[(L,Seq
       val baseScore = base.initialValueForFeature(baseF) //+ math.log(x.foldLeft(1.)(_ + 3 * _))
       assert(!baseScore.isNaN,baseF)
       //baseScore + math.log(1.0 - 1E-10 + math.random * 2 * 1E-10)
-      val r = baseScore + math.log(1.0 - .01 + math.random * 2 * .01)
-      assert(!r.isNaN,"post random: " + baseF)
+      val r = baseScore + math.log(1.0 - .1 + math.random * 2 * .1)
       r
     case _ => 0.0
   }
