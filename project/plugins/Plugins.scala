946d95b6dae45480d081652a4917e53a76d1b016
David Hall <dlwh@berkeley.edu>
2012-01-10 13:01:10 -0800

    forgot sbt files all this time.

---
+++ project/plugins/Plugins.scala
@@ -0,0 +1,6 @@
+class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
+  val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
+  val assemblySBT = "com.codahale" % "assembly-sbt" % "0.1.1"
+}
+
+
