b5f30313caabac2e96a7fa607c4797b5da5e16b6
David Hall <dlwh@berkeley.edu>
2012-03-12 16:23:06 -0700

    much better simplecombinepipeline. 91.98 <= 40 on dev

--- src/main/scala/scalanlp/parser/combine/SimpleCombinePipeline.scala
+++ src/main/scala/scalanlp/parser/combine/SimpleCombinePipeline.scala
@@ -2,11 +2,12 @@ package scalanlp.parser
 package combine
 
 import collection.immutable.IndexedSeq
-import scalanlp.trees.BinarizedTree
 import scalanlp.collection.mutable.TriangularArray
 import scalanlp.util.Index
 import scalanlp.tensor.sparse.OldSparseVector
-import projections.LabeledSpanScorer
+import projections.AnchoredRuleProjector.AnchoredData
+import scalanlp.trees._
+import projections.{AnchoredRuleScorer, LabeledSpanScorer}
 
 /**
  * 
@@ -21,24 +22,28 @@ object SimpleCombinePipeline extends CombinePipeline {
       val allTrees = goldTrees.flatMap(_.treeInstances(withGold=false)).toArray ++ trainTrees.flatMap(_.treeInstances(withGold=true))
       val (lexicon,grammar) = GenerativeParser.extractLexiconAndGrammar(allTrees)
       // erase rule counts
-      val zeroParser = new CKYChartBuilder("ROOT", lexicon, grammar, ParseChart.viterbi)
+//      val zeroParser = new CKYChartBuilder("ROOT", lexicon, grammar, ParseChart.viterbi)
+      val zeroParser = new CKYChartBuilder("ROOT", new ZeroLexicon(lexicon), Grammar.zero(grammar), ParseChart.viterbi)
       zeroParser
     }
 
-    val sentToScorerMap = { for {
-      tb <- goldTrees.par
-    } yield {
-      val spans = LabeledSpanExtractor.extractSpans(basicParser.grammar.labelIndex, tb.outputs.values)
-      tb.words -> new LabeledSpanScorer[String](spans)
-    }}.seq.toMap
+    val sentToDataMap = goldTrees.iterator.map(tb => tb.words -> (Map.empty ++ tb.outputs)).toMap
+
+    def sentToScorer(s: Seq[String]) = {
+      val outputs = sentToDataMap(s)
+      val data = LabeledSpanExtractor.extractAnchoredRules(basicParser.grammar.labelIndex, basicParser.grammar.index, outputs.values)
+      new AnchoredRuleScorer[String](data.spanScores, data.unaryScores, data.binaryScores)
+    }
 
     val parser = new Parser[String,String] with Serializable {
       def bestParse(s: Seq[String], spanScorer: SpanScorer[String]) = {
-        val scorer = SpanScorer.sum(sentToScorerMap(s),spanScorer)
+        val scorer = SpanScorer.sum(sentToScorer(s),spanScorer)
         val inside = basicParser.buildInsideChart(s, scorer)
-        decoder.extractBestParse(basicParser.root, basicParser.grammar, inside, null, s, scorer)
+        val outside = basicParser.buildOutsideChart(inside, scorer)
+        decoder.extractBestParse(basicParser.root, basicParser.grammar, inside, outside, s, scorer)
       }
-      val decoder = new SimpleViterbiDecoder[String,String](basicParser.grammar)
+//      val decoder = new SimpleViterbiDecoder[String,String](basicParser.grammar)
+      val decoder = MaxConstituentDecoder.simple[String,String](basicParser.grammar)
     }
     Iterator(("MaxRecall" -> parser ))
   }
@@ -47,7 +52,7 @@ object SimpleCombinePipeline extends CombinePipeline {
 
 
 object LabeledSpanExtractor {
-  /** Returns a triangular array of counts of particular labels showing up on certain spans */
+  /** Returns a triangular array of counts of particular labels showing up on certain spans. 0 counts are reprseented by -Infinity */
   def extractSpans[L](labelIndex: Index[L], trees: Iterable[BinarizedTree[L]]):Array[OldSparseVector] = {
     val arr = TriangularArray.raw(trees.head.span.end+1, null:OldSparseVector)
     // skip unaries.
@@ -56,12 +61,101 @@ object LabeledSpanExtractor {
       val ind = TriangularArray.index(c.span.start,c.span.end)
       var vec = arr(ind)
       if(vec eq null) {
-        vec = new OldSparseVector(labelIndex.size, 0.0, 1)
+        vec = new OldSparseVector(labelIndex.size, Double.NegativeInfinity, 1)
         arr(ind) = vec
+      }
+      if(vec(l) < 0.0) {
+        vec(l)  = 0.0
       }
       vec(l) += 1
     }
     arr
+  }
+
+  /** Returns a AnchoredData with counts of particular *anchored* rules showing up. 0 counts are -Inf.
+   * Nulls are used liberally  for spans without counts*/
+  def extractAnchoredRules[L](labelIndex: Index[L],
+                              ruleIndex: Index[Rule[L]],
+                              trees: Iterable[BinarizedTree[L]]):AnchoredData = {
+    val length = trees.head.span.end
+    // preliminaries: we're not going to fill in everything: some things will be null.
+    // all of this is how to deal with it.
+    val numProjectedLabels = labelIndex.size
+    val numProjectedRules = ruleIndex.size
+    def projFill[T>:Null<:AnyRef:ClassManifest]() = Array.fill[T](numProjectedLabels)(null)
+    def projVector() = {
+      new OldSparseVector(numProjectedLabels, 0.0)
+    }
+
+    def projRuleVector() = {
+      new OldSparseVector(numProjectedRules, 0.0)
+    }
+
+    def getOrElseUpdate[T<:AnyRef](arr: Array[T], i: Int, t : =>T) = {
+      if(arr(i) == null) {
+        arr(i) = t
+      }
+      arr(i)
+    }
+
+    // The data, and initialization. most things init'd to null
+    val lexicalScores = TriangularArray.raw(length+1,null:OldSparseVector)
+    val unaryScores = TriangularArray.raw(length+1,null:OldSparseVector)
+
+    val totals = TriangularArray.raw(length+1,null:OldSparseVector)
+    val totalsUnaries = TriangularArray.raw(length+1,null:OldSparseVector)
+
+    val binaryScores = TriangularArray.raw[Array[OldSparseVector]](length+1,null);
+
+    for(t <- trees; child <- t.allChildren) child match {
+      case NullaryTree(a) =>
+        val ind = TriangularArray.index(child.span.start,child.span.end)
+        val pL = labelIndex(a)
+        getOrElseUpdate(lexicalScores,ind,projVector())(pL) += 1
+      case BinaryTree(a,bc@Tree(b,_),Tree(c, _)) =>
+        val rule = BinaryRule(a,b,c)
+        val pR = ruleIndex(rule)
+
+        val l = labelIndex(a)
+        val index = TriangularArray.index(child.span.start,child.span.end)
+        val innerIndex = bc.span.end - bc.span.start
+        val parentArray =  if(binaryScores(index) eq null) {
+          val numSplits = child.span.end - child.span.start
+          binaryScores(index) = Array.fill(numSplits)(null:OldSparseVector)
+          binaryScores(index)(innerIndex) = projRuleVector()
+          binaryScores(index)(innerIndex)
+        } else if(binaryScores(index)(innerIndex) eq null) {
+          binaryScores(index)(innerIndex) = projRuleVector()
+          binaryScores(index)(innerIndex)
+        } else {
+          binaryScores(index)(innerIndex)
+        }
+        parentArray(pR) += 1
+        if(totals(index) eq null) {
+          totals(index) = projVector;
+        }
+        totals(index)(l) += 1
+        getOrElseUpdate(lexicalScores,index,projVector())(l) = 0
+      case UnaryTree(a,bc@Tree(b,_)) =>
+        val rule = UnaryRule(a,b)
+        val pR = ruleIndex(rule)
+
+        val l = labelIndex(a)
+        val index = TriangularArray.index(child.span.start,child.span.end)
+        val parentArray = if(unaryScores(index) eq null) {
+          unaryScores(index) = projRuleVector()
+          unaryScores(index)
+        } else {
+          unaryScores(index)
+        }
+        parentArray(pR) += 1.0
+        if(totalsUnaries(index) eq null) {
+          totalsUnaries(index) = projVector;
+        }
+        totalsUnaries(index)(l) += 1.0
+
+    }
+    new AnchoredData(lexicalScores, unaryScores, totalsUnaries, binaryScores, totals);
   }
 }
 
