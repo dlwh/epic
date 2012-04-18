f7fc6efb074e52fae11bbf9bfa2ac8ea2c67c0c8
David Hall <dlwhall@stanford.edu>
2012-04-18 15:15:46 -0700

    Update src/main/scala/scalanlp/parser/AnchoredSpanVisitor.scala

--- src/main/scala/scalanlp/parser/AnchoredSpanVisitor.scala
+++ src/main/scala/scalanlp/parser/AnchoredSpanVisitor.scala
@@ -13,6 +13,7 @@ trait AnchoredSpanVisitor {
   def visitBinaryRule(begin: Int, split: Int, end: Int, rule: Int, score: Double)
   def visitUnaryRule(begin: Int, end: Int, rule: Int, score: Double)
   def visitSpan(begin: Int, end: Int, tag: Int, score: Double)
+  
 }
 
 object AnchoredSpanVisitor {
