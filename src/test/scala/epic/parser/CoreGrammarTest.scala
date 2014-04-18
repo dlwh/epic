e6e2c8e3f15d691c0e144769341cf6661d510c90
David Hall <dlwh@cs.berkeley.edu>
2014-04-17 19:25:03 -0700

    i don't remmeber

--- src/test/scala/epic/parser/CoreGrammarTest.scala
+++ src/test/scala/epic/parser/CoreGrammarTest.scala
@@ -20,34 +20,5 @@ class CoreGrammarTest extends FunSuite {
     val in = oin.readObject()
   }
 
-  test("???")  {
-    val g = new X.Foo
-    val bos = new ByteArrayOutputStream()
-    val oos = new ObjectOutputStream(bos)
-    oos.writeObject(g)
-    oos.close()
-    val in = new ObjectInputStream(new ByteArrayInputStream(bos.toByteArray)).readObject()
-    assert(in == g)
-
-  }
-
-
 }
 
-object X {
-  @SerialVersionUID(1L)
-  case class Foo() extends Serializable {
-    @throws(classOf[ObjectStreamException])
-    private def writeReplace():Object = {
-      new Bar()
-    }
-  }
-
-  @SerialVersionUID(1L)
-  class Bar() extends Serializable {
-    @throws(classOf[ObjectStreamException])
-    private def readResolve():Object = {
-      new Foo()
-    }
-  }
-}
