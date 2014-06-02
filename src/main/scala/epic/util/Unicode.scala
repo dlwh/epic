88dd433aeb8a549bd2f64209fa856e33c0a02baf
David Hall <dlwh@cs.berkeley.edu>
2014-06-02 16:49:52 -0700

    more cruft deletion

--- src/main/scala/epic/util/Unicode.scala
+++ src/main/scala/epic/util/Unicode.scala
@@ -13,7 +13,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
-package chalk.text
+package epic.util
 
 /**
  * Utilities for querying unicode code-points.  These supplement the
