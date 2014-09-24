71060bc3587e70effd864b0d43efccc9f48e61a8
Simon Hafner <hafnersimon@gmail.com>
2014-09-25 03:05:00 +0500

    moved the shapeless slab to Slab.scala

--- src/main/scala/epic/slab/Slab.scala
+++ src/main/scala/epic/slab/Slab.scala
@@ -27,14 +27,29 @@ case class EntityMention(entityType: String, id: Option[String] = None) extends 
 
 
 /**
- * A Slab is the core "document" type in Epic. It represents a
- * document and a set of annotations on that document, such as
- * sentence boundaries, tokens, named entity spans, etc. The
- * ContentType is the type of the document--typically a string--and
- * the AnnotationTypes parameter is an encoding of the kinds of
- * annotations that are present. [[epic.slab.AnalysisFunction]]s can
- * be used to add new annotations to a Slab that have the prerequisite
- * annotations.
- * @tparam ContentType
- * @tparam AnnotationTypes
+ * A Slab is the core "document" type in Epic.
  */
+
+import shapeless._
+import shapeless.syntax.typeable._
+import Utils._
+import ops.hlist._
+
+class Slab[Content, L <: HList](val content: Content, val annotations: L) {
+
+  def get[T](index: Int)(implicit sel: Selector[L, Vector[T]]): T = sel(annotations)(index)
+  def get[T](implicit sel: Selector[L, Vector[T]]): Vector[T] = sel(annotations)
+
+  def add[A, Tmp <: HList, Result <: HList](newAnnotations: Vector[A])(implicit adder: Adder.Aux[L, A, Vector[A], Result]): Slab[Content, Result] = {
+    new Slab(content, adder(annotations, newAnnotations))
+  }
+  def add[A, Tmp <: HList, Result <: HList](newAnnotation: A)(implicit adder: Adder.Aux[L, A, Vector[A], Result]): Slab[Content, Result] = {
+    new Slab(content, adder(annotations, Vector(newAnnotation)))
+  }
+
+  def toHList: L = this.annotations
+}
+
+object Slab {
+  def apply[C](content: C): Slab[C, HNil] = new Slab[C, HNil](content, HNil)
+}
