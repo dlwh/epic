3e22cc05230b44b0e2d6a92dd30c4ea300adfdb4
Simon Hafner <hafnersimon@gmail.com>
2014-11-04 07:37:27 -0600

    tests are green again

--- src/test/scala/epic/slab/AnalysisFunction.scala
+++ src/test/scala/epic/slab/AnalysisFunction.scala
@@ -3,6 +3,7 @@ import epic.slab._
 import shapeless._
 import ops.hlist._
 import Utils._
+import epic.trees.Span
 
 import org.scalatest.FunSpec
 
@@ -10,25 +11,26 @@ import org.scalatest.FunSpec
   * A simple regex tokenizer.
   */
 object RegexTokenizer11 extends AnalysisFunction11[String, Sentence, Token] {
-  def apply[In <: HList, Out <: HList](slab: Slab[String, In])(implicit sel: Selector[In, Vector[Sentence]], adder: Adder.Aux[In, Token, Vector[Token], Out]): Slab[String, Out] =
-    slab.add(slab.get[Sentence](sel).flatMap { sentence =>
+  def apply(content: String, sentences: Vector[Sentence]): Vector[Token] = {
+    sentences.flatMap { sentence =>
       "\\p{L}+|\\p{P}+|\\p{N}+".r.findAllMatchIn(
-        slab.content.substring(sentence.begin, sentence.end)
+        content.substring(sentence.begin, sentence.end)
       ).map(
-        m => Token(sentence.begin + m.start, sentence.begin + m.end)
+        m => Token(Span(sentence.begin + m.start, sentence.begin + m.end))
       ).toVector
-    })(adder)
+    }
+  }
 }
 
 // Same again, except using a different interface. The API sucks,
 // needs to be improved.
 object RegexTokenizerN1 extends AnalysisFunctionN1[String, Vector[Sentence] :: HNil, Token] {
   def apply[In <: HList, Out <: HList](slab: Slab[String, In])(implicit sel: SelectMany.Aux[In, Vector[Sentence] :: HNil, Vector[Sentence] :: HNil], adder: Adder.Aux[In, Token, Vector[Token], Out]): Slab[String, Out] =
-    slab.add(slab.getMany[Vector[Sentence] :: HNil](sel).at(0).flatMap { sentence =>
+    slab.add(slab.selectMany[Vector[Sentence] :: HNil](sel).at(0).flatMap { sentence =>
       "\\p{L}+|\\p{P}+|\\p{N}+".r.findAllMatchIn(
         slab.content.substring(sentence.begin, sentence.end)
       ).map(
-        m => Token(sentence.begin + m.start, sentence.begin + m.end)
+        m => Token(Span(sentence.begin + m.start, sentence.begin + m.end))
       ).toVector
     })(adder)
 }
@@ -38,17 +40,17 @@ class SimpleTokenizerTest extends FunSpec {
   describe("a 1 to 1 tokenizer") {
     val tokenizer = RegexTokenizer11
     val string = "A simple sentence to be analyzed."
-    val slab = Slab(string, Vector(Sentence(0, string.length - 1)) :: HNil)
+    val slab = Slab(string, Vector(Sentence(Span(0, string.length - 1))) :: HNil)
     it("should annotate tokens") {
-      assert(tokenizer(slab).get[Token] == Vector(Token(0,1), Token(2,8), Token(9,17), Token(18,20), Token(21,23), Token(24,32)))
+      assert(tokenizer(slab).select[Token] == Vector(Token(Span(0,1)), Token(Span(2,8)), Token(Span(9,17)), Token(Span(18,20)), Token(Span(21,23)), Token(Span(24,32))))
     }
   }
   describe("a N to 1 tokenizer") {
     val tokenizer = RegexTokenizerN1
     val string = "A simple sentence to be analyzed."
-    val slab = Slab(string, Vector(Sentence(0, string.length - 1)) :: HNil)
+    val slab = Slab(string, Vector(Sentence(Span(0, string.length - 1))) :: HNil)
     it("should annotate tokens") {
-      assert(tokenizer(slab).get[Token] == Vector(Token(0,1), Token(2,8), Token(9,17), Token(18,20), Token(21,23), Token(24,32)))
+      assert(tokenizer(slab).select[Token] == Vector(Token(Span(0,1)), Token(Span(2,8)), Token(Span(9,17)), Token(Span(18,20)), Token(Span(21,23)), Token(Span(24,32))))
     }
   }
 }
--- src/test/scala/epic/slab/SlabTest.scala
+++ src/test/scala/epic/slab/SlabTest.scala
@@ -15,15 +15,15 @@ class ShapelessSlabTest extends FunSpec {
     val annotation = new Annotation {}
     it("should store an annotation") {
       val slab2 = slab.add(annotation)
-      assert(slab2.get[Annotation](0) === annotation)
+      assert(slab2.select[Annotation](0) === annotation)
     }
     it("should add further annotations to the same vector, with disturbing the order") {
       case class Foo(foo: Int) extends Annotation
       val slab2 = slab.add(Foo(13))
-      assert(slab2.get[Foo](0) === Foo(13))
+      assert(slab2.select[Foo](0) === Foo(13))
       val slab3 = slab2.add(Foo(42))
-      assert(slab3.get[Foo](0) === Foo(13))
-      assert(slab3.get[Foo](1) === Foo(42))
+      assert(slab3.select[Foo](0) === Foo(13))
+      assert(slab3.select[Foo](1) === Foo(42))
     }
     it("should raise a type error if trying to retrieve that's not available") {
       case class Foo(foo: Int) extends Annotation
@@ -37,7 +37,7 @@ class ShapelessSlabTest extends FunSpec {
     it("should also work with empty annotation lists") {
       case class Foo(foo: Int) extends Annotation
       val slab1 = slab.add(List[Int]().map(Foo(_)).toVector)
-      slab1.get[Foo] == Vector[Foo]()
+      slab1.select[Foo] == Vector[Foo]()
     }
   }
 }
