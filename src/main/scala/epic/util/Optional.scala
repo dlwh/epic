c149e6effc5614044bd995dbf52aede0b970ea25
David Hall <dlwh@cs.berkeley.edu>
2015-06-17 14:23:22 -0700

    revert Optional, since breeze-config can't deal with the new form.

--- src/main/scala/epic/util/Optional.scala
+++ src/main/scala/epic/util/Optional.scala
@@ -5,16 +5,23 @@ package epic.util
  * Mostly for use in optional argument lists.
  * @author dlwh, jovilius
  */
-case class Optional[+A](value: Option[A]) {
-  def foldLeft[B](b: B)(f: (B, A) => B) = value.fold(b)(x => f(b, x))
+sealed trait Optional[+A] {
+  def foldLeft[B](b: B)(f: (B, A) => B) = toOption.foldLeft(b)(f)
+
+  def toOption = this match {
+    case NotProvided => None
+    case Provided(v) => Some(v)
+  }
 }
+case class Provided[A](value: A) extends Optional[A]
+case object NotProvided extends Optional[Nothing]
 
 object Optional {
 
-  implicit def anyToOptional[A](x: A): Optional[A] = if (x == null) Optional(None) else Optional(Some(x))
+  implicit def anyToOptional[A](x: A): Optional[A] = if (x == null) NotProvided else Provided(x)
 
-  implicit def optionToOptional[A](x: Option[A]): Optional[A] = Optional(x)
+  implicit def optionToOptional[A](x: Option[A]): Optional[A] = x.fold(NotProvided:Optional[A])(Provided(_))
 
-  implicit def optionalToOption[A](x: Optional[A]): Option[A] = x.value
+  implicit def optionalToOption[A](x: Optional[A]): Option[A] = x.toOption
   
 }
