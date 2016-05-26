78f9df038a823f9e7d3dcfc2b1790d907ffb44df
adpauls <adpauls@semanticmachines.com>
2016-05-26 13:56:40 -0700

    Cleanup

--- src/main/scala/epic/logo/ObjectiveFunctionConvergenceChecker.scala
+++ src/main/scala/epic/logo/ObjectiveFunctionConvergenceChecker.scala
@@ -2,13 +2,15 @@ package epic.logo
 
 import scala.collection.Seq
 
-class ObjectiveFunctionConvergenceChecker[W](val objective : ObjectiveFunction[W], val maxNumIters : Int, val callback : IterationCallback[_, W], val tol: Double) extends ConvergenceChecker[W] {
+case class ObjectiveFunctionConvergenceChecker[W](
+    objective: ObjectiveFunction[W], maxNumIters: Int,
+    callback: IterationCallback[_, W], tol: Double) extends ConvergenceChecker[W] {
 
-  def converged(weights : Weights[W], data : Seq[Instance[_, W]], iter : Int, numNewConstraints : Int) : Boolean = {
+  def converged(weights: Weights[W], data: Seq[Instance[_, W]], iter: Int, numNewConstraints: Int): Boolean = {
     iter >= maxNumIters || (objectiveConverged(weights, data, iter) && numNewConstraints == 0)
   }
 
-  private def objectiveConverged(weights : Weights[W], data : Seq[Instance[_, W]], iter : Int) = {
+  private def objectiveConverged(weights: Weights[W], data: Seq[Instance[_, W]], iter: Int) = {
     val (primal, dual) = objective.calculatePrimalAndDual(weights, data)
     callback.objectiveValCheck(primal, dual, iter, weights)
     NumUtils.approxEquals(primal, dual, 1e-10)
